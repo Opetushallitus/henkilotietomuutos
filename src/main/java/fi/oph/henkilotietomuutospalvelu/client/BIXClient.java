1c59577c97dd216939aa0334fd3542b561582390
Pasi Kallioniemi <pasi.kallioniemi@gofore.com>
2018-09-11 10:17:26 +0300

    Clean up logging and return to WARN level for sftp client

--- src/main/java/fi/oph/henkilotietomuutospalvelu/client/BIXClient.java
+++ src/main/java/fi/oph/henkilotietomuutospalvelu/client/BIXClient.java
@@ -87,11 +87,6 @@ public class BIXClient implements Closeable {
                 repository.add(getHostKey(ftpProperties.getHost()), new UserInfoDebugger());
             }
 
-
-            if(repository.getHostKey() != null) {
-                log.info("using hostkeys:" + Arrays.stream(repository.getHostKey()).map(HostKey::getHost).collect(Collectors.joining(",")));
-            }
-
             Session session = jSch.getSession(ftpProperties.getUser(), ftpProperties.getHost(), Integer.valueOf(ftpProperties.getPort()));
             session.setConfig("StrictHostKeyChecking", "yes");
             session.setPassword(ftpProperties.getPassword());
