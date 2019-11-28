dd89e6cc14a4750b09861577c0a3b352d002c147
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-11-28 14:37:04 +0200

    Korjaa typo
    KJHH-1738

--- src/main/resources/db/migration/V2019112712044785473__lapsihuoltolakimuutos.sql
+++ src/main/resources/db/migration/V2019112712044785473__lapsihuoltolakimuutos.sql
@@ -4,7 +4,7 @@ alter table tietoryhma drop column resolution_date;
 alter table tietoryhma drop column voimassa;
 
 alter table tietoryhma add column asuminen varchar(255);
-alter table tietoryhma add column asuminen_alkupvm date,
+alter table tietoryhma add column asuminen_alkupvm date;
 alter table tietoryhma add column asuminen_loppupvm date;
 alter table tietoryhma add column laji varchar(255);
 alter table tietoryhma add column rooli varchar(255);
