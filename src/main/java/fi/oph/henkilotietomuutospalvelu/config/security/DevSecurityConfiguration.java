037507e1a3b5b10752ad1627dc18e9ba72d9dd36
Henry Heikkinen <henry.heikkinen@reaktor.com>
2022-12-19 14:06:46 +0200

    Update locally used static password format to work with current Spring version

--- src/main/java/fi/oph/henkilotietomuutospalvelu/config/security/DevSecurityConfiguration.java
+++ src/main/java/fi/oph/henkilotietomuutospalvelu/config/security/DevSecurityConfiguration.java
@@ -25,6 +25,6 @@ public class DevSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("oph").password("oph").roles("APP_HENKILOTIETOMUUTOS_REKISTERINPITAJA");
+        auth.inMemoryAuthentication().withUser("oph").password("{noop}oph").roles("APP_HENKILOTIETOMUUTOS_REKISTERINPITAJA");
     }
 }
